44dd512a1983f3c2486bacd50e2c7a41c80b9bd6
Shaimaa Natshah <77439067+ShaimaaNatshah@users.noreply.github.com>
2023-05-22 20:42:29 +0300

    Add Github action to Webshipco automating testing add Github action

--- assets/config_templates/circleci-linux-chrome.conf.js
+++ assets/config_templates/circleci-linux-chrome.conf.js
@@ -22,7 +22,7 @@ module.exports = {
 
   test_settings: {
     default: {
-      launch_url: 'http://localhost:8080/',
+      launch_url: 'https://webship.co',
       selenium_port: 4444,
       selenium_host: '127.0.0.1',
       silent: true,
