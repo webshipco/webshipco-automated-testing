e995a1b16505b27db515fbe69aa983342a341e05
Shaimaa Natshah <shaimaa.natshah11@gmail.com>
2023-07-21 20:54:09 +0300

    WPORTAL-79: Add automated testing for header and footer

--- tests/step-definitions/custom.js
+++ tests/step-definitions/custom.js
@@ -3,16 +3,18 @@ const {When, Before} = require('@cucumber/cucumber');
 const {Then} = require('@cucumber/cucumber');
 
 /**
-* Verify, that current page path is equal to specified
-* Example: Then I should be on "/"
-* Example: And I should be on "/user/login"
-* Example: And I should be on "http://google.com"
-*
-* @Then /^I should be on "([^"]*)?"$/
+* Verify, that current page header is working
+* Example: Then I should have a working header
 */
+
 Then(/^I should have a working header$/, function () {
   return browser.smartTestHeader();
 });
+
+/**
+* Verify, that current page footer is working
+* Example: Then I should have a working footer
+*/
 
 Then(/^I should have a working footer$/, function () {
   return browser.smartTestFooter();
