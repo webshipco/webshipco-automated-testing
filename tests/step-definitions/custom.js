f23e21b58907060adf9a4b33d787159b8e6bf854
Shaimaa Natshah <shaimaa.natshah11@gmail.com>
2024-07-23 10:38:23 +0300

    Fix Automated testing

--- tests/step-definitions/custom.js
+++ tests/step-definitions/custom.js
@@ -6,14 +6,45 @@ const {Then} = require('@cucumber/cucumber');
 */
 
 Then(/^I should have a working header$/, function () {
-  return browser.assert.haveWorkingHeader();
-});
-
-/**
-* Verify, that current page footer is working
-* Example: Then I should have a working footer
-*/
+  return this.shouldSee = function (browser) {
+    browser.assert.textContains('header', "Services")
+      .assert.textContains('header', "Docs")
+      .assert.textContains('header', "Blog")
+      .smartClickLink("Services")
+      .waitForElementPresent('body', 3000)
+      .assert.textContains('.page-title', "Services")
+      .url(currentUrl.value)
+      .smartClickLink("Docs")
+      .waitForElementPresent('body', 3000)
+      .assert.textContains('.page-title', "Docs")
+      .url(currentUrl.value);
+  };
+}); 
 
 Then(/^I should have a working footer$/, function () {
-  return browser.assert.haveWorkingFooter();
+  return this.shouldSee = function (browser) {
+    browser.assert.textContains('footer', "Contact")
+      .assert.textContains('footer', "About us")
+      .assert.textContains('footer', "Privacy")
+      .assert.textContains('footer', "© Copyright 2023 Webship")
+      .assert.textContains('footer', "Github")
+      .assert.textContains('footer', "Gitlab")
+      .assert.textContains('footer', "Bitbucket")
+      .assert.textContains('footer', "Twitter")
+      .assert.textContains('footer', "Linkedin")
+      .assert.textContains('footer', "Youtube")
+      .smartClickLink("Contact")
+      .waitForElementPresent('body', 3000)
+      .assert.textContains('.page-title', "Contact")
+      .url(currentUrl.value)
+      .smartClickLink("About us")
+      .waitForElementPresent('body', 3000)
+      .assert.textContains('.page-title', "About us")
+      .url(currentUrl.value)
+      .smartClickLink("Privacy")
+      .waitForElementPresent('body', 3000)
+      .assert.textContains('.page-title', "Privacy")
+      .url(currentUrl.value);
+  };
 });
+
